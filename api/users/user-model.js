37a67dc7a720ceed4c34d511145af89933bedd37
Ladrillo <gabriel@cabrejas.net>
2021-12-08 20:00:41 +0100

    committing to "lecture"

--- api/users/user-model.js
+++ api/users/user-model.js
@@ -75,9 +75,11 @@ group by user_id;
 async function findById(id) {
   const rows = await db('users as u')
     .leftJoin('posts as p', 'u.id', 'p.user_id')
-    .select('u.id as user_id', 'u.username', 'p.id as post_id')
-    
+    .select('u.id as user_id', 'u.username', 'p.id as post_id', 'contents')
+    .where('u.id', id)
 
+console.log(rows)
+return
   /*
     Improve so it resolves this structure:
 
