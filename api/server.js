cc89f046f45273cf4cb782659beb698d63ed88ef
Benedict Schurwanz <19333755+benedictify@users.noreply.github.com>
2021-12-12 22:00:26 -0600

    b notes

--- api/server.js
+++ api/server.js
@@ -6,6 +6,8 @@ const server = express()
 
 server.use(express.json())
 server.use('/api/users', UserRouter)
+
+// error handler:
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({ message: err.message })
 })
